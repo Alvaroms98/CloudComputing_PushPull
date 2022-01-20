9b96ce27c05853f6b2d790e8de429e9ab352fdc8
alvaro.moure1998 <alvaro.moure1998@gmail.com>
2022-01-20 15:48:56 +0100

    Now front-end has all the functionality, insert, querys and deletes. We need to develop the integration of the new functionality to the rest of the microservices

--- code/frontend/logica.js
+++ code/frontend/logica.js
@@ -30,6 +30,8 @@ class Logica {
         console.log("\n Proxys Conectados!");
     }
 
+    // ------------------------------- POST (WORKER JOB) --------------------------------------
+
     // Se llamará desde un POST del REST API
     // Y pondrá el trabajo en la cola de mensajes
     async guardarObjeto(propietario, objeto){
@@ -48,9 +50,17 @@ class Logica {
         
         let respuesta = await this.proxyworker.llamar(funcion, argumentos);
         
+        let objetoID = respuesta.respuesta.insertId;
+        respuesta = {
+            mensaje: `Objeto insertado en la base de datos correctamente`,
+            identificadorUnico: objetoID
+        }
+
         return respuesta; // ID UNICO
 
     }
+
+    // ------------------------------- GET --------------------------------------
 
     // Saca todas las instancias de la base de datos
     async sacarTodo(){
@@ -85,12 +95,113 @@ class Logica {
         console.log(respuesta);
         return respuesta;
     }
-}
 
-// Delete
-// TODO
-// TODO DE PROPIETARIO
-// TODO QUE TENGA UN CAMPO
-// UN UNICO POST -> ID
+    // Sacar el objeto por ID (este ID se devuelve en la respusta del POST)
+    async buscarPorID(id){
+        const funcion = 'buscarPorID';
+        let argumentos = {
+            id: id
+        };
+        argumentos = JSON.stringify(argumentos);
+
+        let respuesta = await this.proxydb.llamar(funcion, argumentos);
+
+        respuesta = JSON.parse(respuesta);
+        console.log(respuesta);
+        return respuesta;
+    }
+
+    // Busca todos los objetos de un propietario que tengan el campo
+    async buscarObjetoporPropietarioYCampo(propietario, campo){
+        let objetosPropietario = await this.buscarObjetoporPropietario(propietario);
+
+        // Si no se han encontrado objetos del propietario
+        if (objetosPropietario === null){
+            return {mensaje: "No se han encontrado objetos"};
+
+        } else { // Si se encuentran objetos hay que filtrarlos
+            let objetosMatch = [];
+            objetosPropietario.forEach((elemento) => {
+                // Metodo para comprobar si existe un campo
+                if (elemento.MyData.hasOwnProperty(campo)){
+                    objetosMatch.push(elemento);
+                }
+            });
+
+            // Ahora comprobamos la longitud del array de match
+            if (objetosMatch.length === 0){ // Si no hay matches
+                return {mensaje: "No se han encontrado objetos"};
+
+            } else { // Si hay matches los devolvemos directamente
+                return objetosMatch;
+            }
+        }
+    }
+
+
+    // ------------------------------- DELETE (WORKER JOB) --------------------------------------
+
+    // Borra toda la tabla de la base de datos
+    async borrarTodo(){
+        const funcion = 'borrarTodo';
+        let argumentos = JSON.stringify({});
+
+        let respuesta = await this.proxyworker.llamar(funcion, argumentos);
+
+        respuesta = JSON.parse(respuesta);
+        return respuesta;
+    }
+
+    // Borrar todos los objetos de un propietario
+    async borrarPropietario(propietario){
+        const funcion = 'borrarPropietario';
+        let argumentos = {
+            propietario: propietario
+        };
+        argumentos = JSON.parse(argumentos);
+
+        let respuesta = await this.proxyworker.llamar(funcion, argumentos);
+
+        respuesta = JSON.parse(respuesta);
+        return respuesta;
+    }
+
+    // Borrar por ID
+    async borrarPorID(id){
+        const funcion = 'borrarPorID';
+        let argumentos = {
+            id: id
+        };
+
+        argumentos = JSON.parse(argumentos);
+
+        let respuesta = await this.proxyworker.llamar(funcion, argumentos);
+
+        respuesta = JSON.parse(respuesta);
+        return respuesta;
+    }
+
+    // Borrar todos los objetos de un propietario que tengan un campo en especificio
+    async borrarObjetoporPropietarioYCampo(propietario,campo){
+        // Primero buscamos cuales hay
+        let objetosMatch = await this.buscarObjetoporPropietarioYCampo(propietario,campo);
+
+        if (objetosMatch.hasOwnProperty('mensaje')){
+            let noHayMatch = objetosMatch;
+            return noHayMatch;
+        } else{
+            // Extraemos todos los ids de los objetos encontrados y llamamos
+            // en bucle a la funcion borrarPorID
+            objetosMatch.forEach((elemento) => {
+                _ = await this.borrarPorID(elemento.Id);
+            });
+            let respuesta = {
+                mensaje: "Objetos eleminados de la base de datos permanentemente",
+                objetosEliminados: objetosMatch,
+            };
+            return respuesta;
+        }
+    }
+}
 
 module.exports.Logica = Logica;
--- code/frontend/rutasREST/api.js
+++ code/frontend/rutasREST/api.js
@@ -16,11 +16,23 @@ class rutasAPI {
 
         // Todas las rutas han de llamar a la lógica
 
+        // ---------------------- /api/ ------------------------------
+
         // Este debe pedir toda la base de datos
         this.router.get('/', async (req, res) => {
             const respuesta = await logica.sacarTodo();
             res.json(respuesta);
         });
+
+        // borrar toda la base de datos (peligroso)
+        this.router.delete('/', async (req,res) => {
+            const respuesta = await logica.borrarTodo();
+            res.json(respuesta);
+        });
+
+
+
+        // ---------------------- /api/propietario/ --------------------
 
         // Pedir todos los datos de un usuario
         this.router.get('/propietario/:propietario', async (req,res) => {
@@ -41,14 +53,55 @@ class rutasAPI {
             // Comprobamos si el objeto está vacio o si no se está enviando
             // un objeto json
             if ( Object.keys(objeto).length === 0 ){
-                res.json({"mensaje": "error en el formato (json), o objeto vacío."});
+                res.status(404).json({"mensaje": "error en el formato (json), o objeto vacío."});
             } else{
                 const respuesta = await logica.guardarObjeto(propietario, objeto);
                 res.json(respuesta);
             }
-            
-            
         });
+
+        // Eliminar todos los objetos de un propietario
+        this.router.delete('/propietario/:propietario', async (req,res) => {
+            const { propietario } = req.params;
+
+            const respuesta = await logica.borrarPropietario(propietario);
+            res.json(respuesta);
+        });
+
+        // ----------------------- /api/propietario/:propietario/campo/ --------
+
+        // Busca todos los objetos de un propietario que tengan el campo
+        this.router.get('/propietario/:propietario/campo/:campo', async (req,res) => {
+            const { propietario, campo } = req.params;
+
+            const respuesta = await logica.buscarObjetoporPropietarioYCampo(propietario,campo);
+            res.json(respuesta);
+        });
+
+        // Borrar todos los objetos de un propietario que tengan un campo en especificio
+        this.router.delete('/propietario/:propietario/campo/:campo', async (req,res) => {
+            const { propietario, campo } = req.params;
+            const respuesta = await logica.borrarObjetoporPropietarioYCampo(propietario,campo);
+            res.json(respuesta);
+        });
+
+
+        // ----------------------- /api/id/ --------------------------------- 
+
+        // busca un objeto único
+        this.router.get('/id/:id', async (req,res) => {
+            const { id } = req.params;
+            const respuesta = await logica.buscarPorID(id);
+            res.json(respuesta);
+        });
+
+        // borra un objeto unico
+        this.router.delete('/id/:id', async (req,res) => {
+            const { id } = req.params;
+            const respuesta = await logica.borrarPorID(id);
+            res.json(respuesta);
+        });
+
     }
 }
 
